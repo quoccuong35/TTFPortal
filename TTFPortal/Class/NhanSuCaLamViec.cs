b4ea5b46498ef6962110520d4ad19c5a852363b2
cuonglq <CuongLq@bd-cntt-003.truongthanh.com>
2021-05-10 13:44:37 +0700

    NhanSuCaLamViec

---
+++ TTFPortal/Class/NhanSuCaLamViec.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TTFPortal.Models;
+
+namespace TTFPortal.Class
+{
+    public class NhanSuCaLamViec
+    {
+        public int NhanSu { get; set; }
+        public string MaNV { get; set; }
+        public string HoVaTen { get; set; }
+        public string Ngay { get; set; }
+        public string GioVao { get; set; }
+        public string GioRa { get; set; }
+        public string GhiChu { get; set; }
+        public string SQL { get; set; }
+    }
+}
